ddd0642b59b39d02771e45f873bd0c22a6cad229
Yves Scherrer <yves.scherrer@gmail.com>
2018-05-11 09:57:21 +0300

    add detokenization

--- select_shuf/reformat.py
+++ select_shuf/reformat.py
@@ -1,20 +1,24 @@
 #! /usr/bin/env python3
 
-import os, sys
+import os, sys, subprocess
 
 prefix = sys.argv[1]
 suffix = sys.argv[2]
 
 outfile = open("morpheval-enfi-2018.en", 'w', encoding='utf-8')
 
+tokenizer = subprocess.Popen(["/proj/nlpl/software/moses/4.0-65c75ff/moses/scripts/tokenizer/detokenizer.perl", "-l en"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
 for filename in os.listdir("."):
 	if filename.startswith(prefix) and filename.endswith(suffix + ".txt"):
 		print("Processing", filename)
 		infile = open(filename)
-		for line in infile:
+		infile1 = open(filename.replace(".txt", ".1.txt"))
+		infile2 = open(filename.replace(".txt", ".2.txt"))
+		for line, line1, line2 in zip(infile, infile1, infile2):
 			elements = line.split("\t")
-			outfile.write("{}:{}.1\t{}\n".format(elements[2].strip(), elements[3].strip(), elements[0].strip()))
-			outfile.write("{}:{}.2\t{}\n".format(elements[2].strip(), elements[3].strip(), elements[1].strip()))
+			outfile.write("{}:{}.1\t{}\n".format(elements[2].strip(), elements[3].strip(), line1.strip()))
+			outfile.write("{}:{}.2\t{}\n".format(elements[2].strip(), elements[3].strip(), line2.strip()))
 		infile.close()
 	else:
 		print("Skipping file", filename)
--- select_shuf/run.sh
+++ select_shuf/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -l
 
+module use -a /proj/nlpl/software/modulefiles
+module load moses
+TOKENIZER=/proj/nlpl/software/moses/4.0-65c75ff/moses/scripts/tokenizer
+
 DATASET=news2007
 NBLINES=500
 
@@ -8,6 +12,10 @@ do
 	echo $FEATURE
 	if [ ! -e "$DATASET.$FEATURE.$NBLINES.txt" ]; then
 		shuf -n $NBLINES ../score/$DATASET.$FEATURE.filtered.txt > $DATASET.$FEATURE.$NBLINES.txt
+	fi
+	if [ ! -e "$DATASET.$FEATURE.$NBLINES.2.txt" ]; then
+		cut -f 1 $DATASET.$FEATURE.$NBLINES.txt | $TOKENIZER/detokenizer.perl -l en > $DATASET.$FEATURE.$NBLINES.1.txt
+		cut -f 2 $DATASET.$FEATURE.$NBLINES.txt | $TOKENIZER/detokenizer.perl -l en > $DATASET.$FEATURE.$NBLINES.2.txt
 	fi
 done
 
